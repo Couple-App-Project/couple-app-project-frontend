18f909cbda67966aedd9b26ca1b3e1e3a00255e8
정소윤 <USER@jeongsoyun-ui-MacBookPro.local>
2022-11-07 23:56:44 +0900

    [feature]: 로그인페이지 기본 레이아웃

---
+++ pages/login.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Login() {
+  const router = useRouter();
+
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput((prevInput) => {
+      return { ...prevInput, [e.target.name]: e.target.value };
+    });
+  };
+  const handleLogin = () => {
+    console.log(userInput);
+    router.push("/");
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>
+          이메일
+          <input
+            type="email"
+            name="email"
+            value={userInput.email}
+            onChange={changeInput}
+          />
+        </label>
+        <br />
+
+        <label>
+          비밀번호
+          <input
+            type="password"
+            name="password"
+            value={userInput.password}
+            onChange={changeInput}
+          />
+        </label>
+        <br />
+
+        <button onClick={handleLogin}>로그인</button>
+      </form>
+
+      {/* TODO : Link 주소 회원가입 페이지로 변경하기 */}
+      <Link href="/">
+        <button>회원가입</button>
+      </Link>
+    </>
+  );
+}
