0b9755ea84cb43a5243e74ba8a7afe6aa38ef550
choi-sus <choi-sus@naver.com>
2022-11-08 11:53:15 +0900

    [Feat] : 회원가입 커스텀 훅 생성

---
+++ feature/signUp/screen/ScreenSignUp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ScreenSignUp = () => {
+  return <SignUpWrap />;
+};
+
+export default ScreenSignUp;
+
+const SignUpWrap = styled.div``;
