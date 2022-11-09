d0830359634e014bfc26c3f41194c0d345c033d4
choi-sus <choi-sus@naver.com>
2022-11-09 12:25:58 +0900

    [Feat] : 회원가입 markup 추가

--- feature/signUp/screen/ScreenSignUp.tsx
+++ feature/signUp/screen/ScreenSignUp.tsx
@@ -1,10 +1,82 @@
 import React from "react";
 import styled from "styled-components";
+import useInput from "../hooks/useInput";
 
 const ScreenSignUp = () => {
-  return <SignUpWrap />;
+  const [email, onChangeEmail] = useInput("");
+  const [pwd, onChangePwd] = useInput("");
+  const [pwdConfirm, onChangePwdConfirm] = useInput("");
+  const [name, onChangeName] = useInput("");
+  const [birthday, onChangeBirthday] = useInput("");
+
+  const pwdConfirmText = () => {
+    if (pwd !== pwdConfirm) {
+      return { display: "block" };
+    } else {
+      return { display: "none" };
+    }
+  };
+
+  const heandleSignUp = () => {
+    const userSignUp = { email, password: pwd, name, birthday };
+    console.log(userSignUp);
+  };
+
+  return (
+    <SignUpWrapper>
+      <form className="sign-up-content">
+        <fieldset>
+          <legend>회원가입</legend>
+          <div className="formbox">
+            <label htmlFor="email">이메일</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+          <div className="formbox">
+            <label htmlFor="password">비밀번호</label>
+            <input
+              id="password"
+              type="password"
+              value={pwd}
+              onChange={onChangePwd}
+            />
+          </div>
+          <div className="formbox">
+            <label htmlFor="password-confirm">비밀번호 확인</label>
+            <input
+              id="password-confirm"
+              type="password"
+              value={pwdConfirm}
+              onChange={onChangePwdConfirm}
+            />
+            <p style={pwdConfirmText()}>비밀번호가 일치하지 않습니다.</p>
+          </div>
+          <div className="formbox">
+            <label htmlFor="name">이름</label>
+            <input id="name" value={name} onChange={onChangeName} />
+          </div>
+          <div className="formbox">
+            <label htmlFor="birthday">생년월일</label>
+            <input
+              id="birthday"
+              type="date"
+              value={birthday}
+              onChange={onChangeBirthday}
+            />
+          </div>
+          <button type="submit" onClick={heandleSignUp}>
+            회원가입
+          </button>
+        </fieldset>
+      </form>
+    </SignUpWrapper>
+  );
 };
 
 export default ScreenSignUp;
 
-const SignUpWrap = styled.div``;
+const SignUpWrapper = styled.div``;
