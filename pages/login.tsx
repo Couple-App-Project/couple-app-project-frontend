4a59d0ad0639740b98f8fc361b6c326c63ea9c28
정소윤 <USER@jeongsoyun-ui-MacBookPro.local>
2022-11-13 19:44:48 +0900

    [refactor]: 로그인 구조, screen 추가

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,56 +1,5 @@
-import { useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import ScreenLogin from "../feature/Login/screen/ScreenLogin";
 
 export default function Login() {
-  const router = useRouter();
-
-  const [userInput, setUserInput] = useState({
-    email: "",
-    password: "",
-  });
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserInput((prevInput) => {
-      return { ...prevInput, [e.target.name]: e.target.value };
-    });
-  };
-  const handleLogin = () => {
-    console.log(userInput);
-    router.push("/");
-  };
-
-  return (
-    <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label>
-          이메일
-          <input
-            type="email"
-            name="email"
-            value={userInput.email}
-            onChange={onChangeInput}
-          />
-        </label>
-        <br />
-
-        <label>
-          비밀번호
-          <input
-            type="password"
-            name="password"
-            value={userInput.password}
-            onChange={onChangeInput}
-          />
-        </label>
-        <br />
-
-        <button onClick={handleLogin}>로그인</button>
-      </form>
-
-      {/* TODO : Link 주소 회원가입 페이지로 변경하기 */}
-      <Link href="/">
-        <button>회원가입</button>
-      </Link>
-    </>
-  );
+  return <ScreenLogin />;
 }
