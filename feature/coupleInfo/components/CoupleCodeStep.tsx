f7594c56121bd749e8090138bd28e940e8aae502
choi-sus <choi-sus@naver.com>
2022-11-16 10:08:50 +0900

    [Feat] : 커플 코드 컴포넌트 마크업 및 핸들러 추가

--- feature/coupleInfo/components/CoupleCodeStep.tsx
+++ feature/coupleInfo/components/CoupleCodeStep.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, ChangeEvent, useState } from "react";
 import styled from "styled-components";
 
 interface Props {
@@ -6,10 +6,50 @@ interface Props {
 }
 
 const CoupleCodeStep: FunctionComponent<Props> = ({ NextStep }) => {
+  const [coupleCode, setCoupleCode] = useState({
+    userCode: "A98YEON619",
+    loverCode: "",
+  });
+
+  const onChangeCode = (e: ChangeEvent<HTMLInputElement>) => {
+    setCoupleCode((prev) => {
+      return { ...prev, loverCode: e.target.value };
+    });
+  };
+
+  const handlerCopy = () => {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(`${coupleCode.userCode}`)
+        .then(() => alert("코드가 클립보드에 복사되었습니다."))
+        .catch(() => alert("복사를 다시 시도해주세요."));
+    } else {
+      alert("공유하기가 지원되지 않는 환경 입니다.");
+    }
+  };
+
   return (
     <CoupleCodeWrap>
-      <p>커플 코드 입력 페이지</p>
-      <button type="button" onClick={NextStep}>
+      <div>
+        <label>내 커플 코드</label>
+        <input type="text" defaultValue={coupleCode.userCode} disabled />
+        <button type="button" onClick={handlerCopy}>
+          코드복사
+        </button>
+      </div>
+      <div>
+        <label>커플 코드를 받으셨나요?</label>
+        <input
+          type="text"
+          value={coupleCode.loverCode}
+          onChange={onChangeCode}
+        />
+      </div>
+      <button
+        type="button"
+        onClick={NextStep}
+        disabled={coupleCode.loverCode !== "" ? false : true}
+      >
         연결하기
       </button>
     </CoupleCodeWrap>
