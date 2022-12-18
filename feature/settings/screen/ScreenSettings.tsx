4d2f821766a951404e831e91d390602b4f17001e
soyoonJ <sytkfkd96@gmail.com>
2022-12-19 00:59:03 +0900

    [feature]: 설정 페이지 기본

---
+++ feature/settings/screen/ScreenSettings.tsx
@@ -0,0 +1,13 @@
+const ScreenSettings = () => {
+    return (
+        <>
+            <p>프로필사진</p>
+            <p>애칭</p>
+            <p>만난지</p>
+            <p>생일</p>
+            <button>로그아웃</button>
+        </>
+    );
+};
+
+export default ScreenSettings;
