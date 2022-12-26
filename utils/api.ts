94b7bff58b237f33fb7a5ef3c12ab2180c532a70
soyoonJ <sytkfkd96@gmail.com>
2022-12-27 00:52:25 +0900

    [Feat]: .env 추가

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from 'next/router';
 
 const instance = axios.create({
-    baseURL: 'http://ec2-13-125-192-62.ap-northeast-2.compute.amazonaws.com:3000',
+    baseURL: process.env.NEXT_PUBLIC_HOST,
     // headers: {
     //     Authorization: `Bearer ${sessionStorage.getItem('access')}`,
     // },
