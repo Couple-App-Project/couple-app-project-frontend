4b29fba2d29834e7e55cef7b9c1e87df3f21e61e
soyoonJ <sytkfkd96@gmail.com>
2023-01-25 20:47:27 +0900

    [feat]: 캘린더 수정 페이지 생성

---
+++ feature/calender/components/CalendarForm.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { useState } from 'react';
+import useMutationCalendar from '../queries/mutationFn/mutationFn';
+import {useRouter} from 'next/router';
+import styled from 'styled-components';
+import Radio from '../components/Radio';
+
+const Header = styled.header`
+    display: flex;
+`
+const FieldsetStyle = styled.fieldset`
+    all: unset;
+    display: flex;
+`;
+
+
+const CalendarForm = () => {
+    const router = useRouter();
+
+    const createCalendar = useMutationCalendar();
+    const defaultDate = `${new Date().getFullYear()}-${String(new Date().getMonth()+1).padStart(2, '0')}-${String(new Date().getDate()).padStart(2,'0')}`
+
+    const [schedule, setSchedule] = useState({
+        title: '',
+        type: '데이트',
+        startDate: defaultDate,
+        endDate: defaultDate,
+        startTime: "00:00",
+        endTime: "23:59",
+        location: '',
+        content: ''
+
+    });
+
+    const scheduleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        if (name === 'startDateTime') {
+            setSchedule({
+                ...schedule,
+                startDate:value.slice(0,10),
+                startTime:value.slice(-5)
+            })
+        } else if (name === 'endDateTime') {
+            setSchedule({
+                ...schedule,
+                endDate:value.slice(0,10),
+                endTime:value.slice(-5)
+            })
+        } else {
+            setSchedule({
+                ...schedule,
+                [name]: value,
+            });
+        }
+    };
+
+    const postNewSchedule = () => {
+        if (schedule.title==='' || schedule.location==='') alert('모든 정보를 입력해주세요')
+        createCalendar(schedule)
+    }
+
+    return (
+        <div>
+            <Header>
+                <button onClick={() => router.push('/calendar')}>취소</button>
+                {router.pathname==='/calendar/register'?<h1>일정</h1>:<h1>편집</h1>}
+                <button onClick={postNewSchedule}>저장</button>
+            </Header>
+            <br />
+
+            <input
+                placeholder="제목"
+                name="title"
+                value={schedule.title}
+                onChange={scheduleChange}
+            />
+
+            <FieldsetStyle>
+                <legend>카테고리</legend>
+
+                <Radio value="데이트" _onChange={scheduleChange} checked={true}>
+                    데이트
+                </Radio>
+                <Radio value="기념일" _onChange={scheduleChange}>
+                    기념일
+                </Radio>
+            </FieldsetStyle>
+
+            <br />
+            <input
+                type="datetime-local"
+                name="startDateTime"
+                value={schedule.startDate+'T'+schedule.startTime}
+                onChange={scheduleChange}
+            />
+            <input
+                type="datetime-local"
+                name="endDateTime"
+                value={schedule.endDate+'T'+schedule.endTime}
+                onChange={scheduleChange}
+            />
+
+            <br />
+            <input
+                placeholder="장소"
+                name="location"
+                value={schedule.location}
+                onChange={scheduleChange}
+            />
+        </div>
+    );
+};
+
+export default CalendarForm;
