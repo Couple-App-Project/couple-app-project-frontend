eaedc419e571448fe7280279ba4017905383ab2b
choi-sus <choi-sus@naver.com>
2023-01-25 12:24:08 +0900

    [Config] : svg config 세팅 및 설치

--- layouts/StepLayout.tsx
+++ layouts/StepLayout.tsx
@@ -1,3 +1,26 @@
-const StepLayout = () => {};
+import styled from 'styled-components';
+import { ElButton } from 'components';
+
+interface StepLayoutProps {
+    title: string;
+    children: React.ReactNode;
+}
+
+const StepLayout = ({ title, children }: StepLayoutProps) => {
+    return (
+        <StepLayoutWrapper>
+            <div></div>
+            <div>
+                <h2>{title}</h2>
+            </div>
+            <div>{children}</div>
+            <ElButton>
+                {title === '회원가입' ? '가입완료' : '연결하기'}
+            </ElButton>
+        </StepLayoutWrapper>
+    );
+};
 
 export default StepLayout;
+
+const StepLayoutWrapper = styled.div``;
