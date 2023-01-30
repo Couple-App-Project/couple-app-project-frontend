164522e506a026c17729cfcee1013e048266056e
choi-sus <choi-sus@naver.com>
2023-01-30 10:57:53 +0900

    [Feat] : color theme 추가

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,7 +1,19 @@
-export const colors = {
-    primaryPink: '#FF6E7F',
+import { DefaultTheme } from 'styled-components';
+
+const defaultTheme: DefaultTheme = {
+    primaryPink: '#ff6e7f',
+    mediumPink: '#fadfe2',
+    softPink: '#fff0f2',
     primaryBlue: '#48CFE1',
-    font1: '#3B3D49',
-    font2: '#5C5F6D',
-    font3: '#A8AAB2',
+    red: '#fa1c37',
+    black: '#000',
+    white: '#fff',
+    grey_6: '#3b3d49',
+    grey_5: '#5c5f6d',
+    grey_4: '#a8aab2',
+    grey_3: '#e2e4ea',
+    grey_2: '#efefef',
+    grey_1: '#f5f5f5',
 };
+
+export default defaultTheme;
