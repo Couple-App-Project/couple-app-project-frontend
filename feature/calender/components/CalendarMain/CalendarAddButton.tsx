a7ff4f329de592546f0876cf3aab5fb94061ec01
choi-sus <choi-sus@naver.com>
2023-02-08 16:06:53 +0900

    [Feat] : calendar 추가 버튼 추가

--- feature/calender/components/CalendarMain/CalendarAddButton.tsx
+++ feature/calender/components/CalendarMain/CalendarAddButton.tsx
@@ -1,9 +1,22 @@
 import styled from 'styled-components';
+import AddBtnIcon from 'public/images/icons/add-button.svg';
 
 const CalendarAddButton = () => {
-    return <AddBtn>버튼</AddBtn>;
+    return (
+        <AddBtn>
+            <AddBtnIcon />
+        </AddBtn>
+    );
 };
 
 export default CalendarAddButton;
 
-const AddBtn = styled.button``;
+const AddBtn = styled.button`
+    position: absolute;
+    bottom: 6rem;
+    right: 1rem;
+    width: 3rem;
+    height: 3rem;
+    background-color: #ff6e7f;
+    border-radius: 50%;
+`;
