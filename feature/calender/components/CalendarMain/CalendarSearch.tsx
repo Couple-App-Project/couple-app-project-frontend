700250df2f6ac07461617e5fe85cc51218e0725e
choi-sus <choi-sus@naver.com>
2023-02-13 10:17:49 +0900

    [Feat] : search componnent 추가

--- feature/calender/components/CalendarMain/CalendarSearch.tsx
+++ feature/calender/components/CalendarMain/CalendarSearch.tsx
@@ -1,9 +1,28 @@
+import styled from 'styled-components';
+import Back from 'public/images/icons/arrow-back.svg';
+import { useSetRecoilState } from 'recoil';
+import isSearchState from 'recoil/isSearchState';
+
 const CalendarSearch = () => {
+    const isSearch = useSetRecoilState(isSearchState);
     return (
-        <div>
-            <input type="text" />
-        </div>
+        <SearchContainer>
+            <div className="search-input">
+                <div onClick={() => isSearch('')}>
+                    <Back />
+                </div>
+                <input type="text" />
+            </div>
+        </SearchContainer>
     );
 };
 
 export default CalendarSearch;
+
+const SearchContainer = styled.div`
+    position: absolute;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    background: ${(props) => props.theme.white};
+`;
