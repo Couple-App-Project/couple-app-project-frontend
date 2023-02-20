54982c5751e41127c6aa4b58331aa3dfef8c2d1a
choi-sus <choi-sus@naver.com>
2023-02-20 11:32:01 +0900

    [Style] : search type button ui 분기 완료

--- feature/calender/components/CalendarSearch/SearchType.tsx
+++ feature/calender/components/CalendarSearch/SearchType.tsx
@@ -7,7 +7,7 @@ const SearchType = ({ search, onChangeSearch }: CalendarSearchPropsType) => {
             <button
                 name="type"
                 value="데이트"
-                className="date"
+                className={search === '데이트' ? 'date' : ''}
                 onClick={(e) => onChangeSearch(e)}
             >
                 데이트
@@ -15,7 +15,7 @@ const SearchType = ({ search, onChangeSearch }: CalendarSearchPropsType) => {
             <button
                 name="type"
                 value="기념일"
-                className="anniversary"
+                className={search === '기념일' ? 'anniversary' : ''}
                 onClick={(e) => onChangeSearch(e)}
             >
                 기념일
@@ -26,4 +26,27 @@ const SearchType = ({ search, onChangeSearch }: CalendarSearchPropsType) => {
 
 export default SearchType;
 
-const SearchTypeContainer = styled.div``;
+const SearchTypeContainer = styled.div`
+    button {
+        width: 4.875rem;
+        padding: 0.375rem 0;
+        border-radius: 62px;
+        border: 1px solid ${(props) => props.theme.grey_2};
+        background-color: ${(props) => props.theme.white};
+        color: ${(props) => props.theme.grey_4};
+        ${(props) => props.theme.Body_1}
+
+        &:last-child {
+            margin-left: 0.5rem;
+        }
+
+        &.date {
+            background-color: ${(props) => props.theme.primaryPink};
+            color: ${(props) => props.theme.white};
+        }
+        &.anniversary {
+            background-color: ${(props) => props.theme.mediumBlue};
+            color: ${(props) => props.theme.white};
+        }
+    }
+`;
