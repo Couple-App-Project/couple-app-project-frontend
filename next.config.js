b0ffaa5eb423345352f658411856509fbdd34a69
choi-sus <choi-sus@naver.com>
2023-03-08 23:08:46 +0900

    Revert "[Config] : next font config 설정"
    This reverts commit aa0b12fcc0d9db3557bbcfd51e3aadd73b735260.

--- next.config.js
+++ next.config.js
@@ -8,22 +8,6 @@ const nextConfig = {
         // Enables the styled-components SWC transform
         styledComponents: true,
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '/public/font', // <-- This is what was helping.
-                        },
-                    },
-                    'css-loader',
-                ],
-            },
-        ],
-    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/i,
