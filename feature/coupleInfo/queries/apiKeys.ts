60e947766b075cb371487ecee703261891161c95
soyoonJ <sytkfkd96@gmail.com>
2023-03-08 19:02:27 +0900

    [feature]: backgroundColor 추가

--- feature/coupleInfo/queries/apiKeys.ts
+++ feature/coupleInfo/queries/apiKeys.ts
@@ -2,8 +2,9 @@ import instance from 'utils/api';
 
 const apiKeys = {
     createCoupleInfo: async (coupleData: {
-        anniversary: string;
-        nickname: string;
+        anniversary?: string;
+        nickname?: string;
+        backgroundColor?: string;
     }) => await instance.post(`/couples/info`, coupleData),
 };
 
