90de7fd584067fa447a6f81ffac044379780997b
choi-sus <choi-sus@naver.com>
2023-04-26 10:44:39 +0900

    [Fix] : calendar detail 수정

--- feature/diary/components/DiaryCard.tsx
+++ feature/diary/components/DiaryCard.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-import useMutationLabel from '../queries/mutationFn/useMutationLabel';
-
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Bookmark from 'public/icons/bookmark.svg';
+import useMutationLabel from '../queries/mutationFn/useMutationLabel';
 
 const DiaryCard = (props: any) => {
     const { diaryInfo } = props;
